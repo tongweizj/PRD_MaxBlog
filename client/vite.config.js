c93f9da4873bdd63bacebf827c1fa60190f61801
Max <tongweizj@gmail.com>
2026-06-09 16:44:01 -0400

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,8 +8,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,      },
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+      },
     },
   },
 });
